f17bc819dabca9aebc141d9d4aae48852bff063a
Alex <baileymk@hotmail.com>
2024-02-09 09:45:18 +0000

    init commit

---
+++ guinness/src/lambda/createReview/app.ts
@@ -0,0 +1,65 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb"
+const client = new DynamoDBClient({})
+const ddbDocClient = DynamoDBDocumentClient.from(client)
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+const tableName = process.env.REVIEW_TABLE_NAME
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        if (event.httpMethod !== "POST") {
+            throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method`)
+        }
+        // All log statements are written to cloudwatch
+        console.info("received:", event)
+
+        // Get id and name from the body of the request
+        const body = JSON.parse(event.body!)
+        const id = String(body.id)
+        const location = body.location
+        const price = body.price
+        const rating = body.rating
+        const longitude = body.longitude
+        const latitude = body.latitude
+
+        //const o = {}
+
+        //Object.keys(JSON.parse(body)).forEach((key)=> {
+        //  o[key] = String(JSON.parse(body)[key])
+        //})
+
+
+        const params = {
+            TableName: tableName,
+            Item: { id: id, location: location, price: price, rating: rating, longitude: longitude, latitude: latitude }
+        }
+
+
+        const data = await ddbDocClient.send(new PutCommand(params))
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Successfully added review',
+                data: data
+            }),
+        };
+    } catch (err) {
+        console.log("error creating review", err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
---
+++ guinness/src/lambda/getReviews/app.ts
@@ -0,0 +1,64 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient, } from "@aws-sdk/lib-dynamodb"
+const client = new DynamoDBClient({})
+const ddbDocClient = DynamoDBDocumentClient.from(client)
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+const tableName = process.env.REVIEW_TABLE_NAME
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        if (event.httpMethod !== "GET") {
+            throw new Error(`postMethod only accepts GET method, you tried: ${event.httpMethod} method`)
+        }
+        // All log statements are written to cloudwatch
+        console.info("received:", event)
+
+        const params = {
+            TableName: tableName,
+        }
+
+        const data = await ddbDocClient.send(new ScanCommand(params))
+        const items = data.Items
+
+        const normal = items?.map((item) => {
+            const obj: { [key: string]: any } = {}
+            for (const [key, value] of Object.entries(item)) {
+                const v = Object.values(value)
+                obj[key] = v
+            }
+            return obj
+        })
+        console.info(`response from: ${event.path} statusCode: ${200} body: ${items}`);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Returning all reviews',
+                items: normal
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            },
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err,
+            }),
+        };
+    }
+}
