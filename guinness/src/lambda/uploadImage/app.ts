1b48d832ba76cdf73ae5a679b6cdd7c1ee97de86
Alex <baileymk@hotmail.com>
2024-02-23 15:39:34 +0000

    comment out for test

--- guinness/src/lambda/uploadImage/app.ts
+++ guinness/src/lambda/uploadImage/app.ts
@@ -10,22 +10,22 @@ export const uploadImage = async(event: APIGatewayProxyEvent)=>{
         if (event.httpMethod !== "POST") {
             throw new Error(`postMethod only accepts POST  method, you tried: ${event.httpMethod} method`)
         }
-        const body = JSON.parse(event.body!)
-        const key = body.key
-        const s3Params = {
-            Bucket: bucketName,
-            Key: key,
-            Expires: 30000,
-            ContentType: 'image/jpeg'
-        }
+        // const body = JSON.parse(event.body!)
+        // const key = body.key
+        // const s3Params = {
+        //     Bucket: bucketName,
+        //     Key: key,
+        //     Expires: 30000,
+        //     ContentType: 'image/jpeg'
+        // 
 
-        let uploadURL = s3.getSignedUrl('putObject', s3Params)
+        // let uploadURL = s3.getSignedUrl('putObject', s3Params)
 
         return {
             status: 200,
             body: JSON.stringify({
-                uploadURL: uploadURL,
-                filename: key,
+                // uploadURL: uploadURL,
+                // filename: key,
                 message: "Successfully created unsigned url to send image to S3 bucket"
             })
             ,
