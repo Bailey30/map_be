afb60da774958c37e17e195027edc5851f0e7e92
Alex <baileymk@hotmail.com>
2024-02-23 15:44:34 +0000

    everything commented out

--- guinness/src/lambda/uploadImage/app.ts
+++ guinness/src/lambda/uploadImage/app.ts
@@ -2,27 +2,27 @@
 // import { APIGatewayProxyEvent } from "aws-lambda"
 // const s3 = new AWS.S3()
 
-const bucketName = process.env.S3_BUCKET_NAME
+// const bucketName = process.env.S3_BUCKET_NAME
 
-export const uploadImage = async(event: any)=>{
+export const uploadImage = async () => {
     return {
         success: true
     }
     // console.log('received:', event)
     // try {
-        // if (event.httpMethod !== "POST") {
-        //     throw new Error(`postMethod only accepts POST  method, you tried: ${event.httpMethod} method`)
-        // }
-        // const body = JSON.parse(event.body!)
-        // const key = body.key
-        // const s3Params = {
-        //     Bucket: bucketName,
-        //     Key: key,
-        //     Expires: 30000,
-        //     ContentType: 'image/jpeg'
-        // 
+    // if (event.httpMethod !== "POST") {
+    //     throw new Error(`postMethod only accepts POST  method, you tried: ${event.httpMethod} method`)
+    // }
+    // const body = JSON.parse(event.body!)
+    // const key = body.key
+    // const s3Params = {
+    //     Bucket: bucketName,
+    //     Key: key,
+    //     Expires: 30000,
+    //     ContentType: 'image/jpeg'
+    // 
 
-        // let uploadURL = s3.getSignedUrl('putObject', s3Params)
+    // let uploadURL = s3.getSignedUrl('putObject', s3Params)
 
     //     return {
     //         status: 200,
