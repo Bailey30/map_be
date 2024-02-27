ae58636909e620323228dc05a3a6ee3fa632823a
Alex <baileymk@hotmail.com>
2024-02-27 12:49:16 +0000

    get image function

---
+++ guinness/src/lambda/getImage/app.ts
@@ -0,0 +1,52 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import {S3Client, GetObjectCommand} from "@aws-sdk/client-s3"
+
+const bucketName = process.env.S3_BUCKET_NAME
+const client = new S3Client({region: "eu-west-2"})
+
+// ALWAYS RETURN "statusCode" not "status"
+
+export const getImage = async (event: any, context: any, callback: any) => {
+    console.log('received:', event)
+    try {
+        // if (event.httpMethod !== "POST") {
+        //     throw new Error(`postMethod only accepts POST  method, you tried: ${event.httpMethod} method`)
+        // }
+
+        const body = JSON.parse(event.body!)
+        const key = body.key
+        const s3Params = {
+            Bucket: bucketName,
+            Key: key,
+            // Expires: 30000,
+            ContentType: 'image/jpeg'
+        }
+
+        const command = new GetObjectCommand(s3Params)
+        const uploadURL = await getSignedUrl(client, command, {expiresIn: 30000})
+
+        callback(null,{
+            statusCode: 200,
+            body: JSON.stringify({
+                uploadURL: uploadURL,
+                filename: key,
+                message: "Successfully created unsigned url to get image from S3 bucket"
+            })
+            ,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
+        })
+
+    } catch (err: any) {
+        console.log("error occuring trying to create unsigned url", err)
+        callback( null, {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Error trying to create unsigned url",
+                error: err,
+            })
+        })
+    }
+
+}
