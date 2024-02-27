acaf81c692a2d44fd935a8bbdc64888effa4f303
Alex <baileymk@hotmail.com>
2024-02-27 10:28:52 +0000

    commented out to see what will return

--- guinness/src/lambda/uploadImage/app.ts
+++ guinness/src/lambda/uploadImage/app.ts
@@ -1,6 +1,6 @@
-import AWS from "aws-sdk"
-import { APIGatewayProxyEvent } from "aws-lambda"
-const s3 = new AWS.S3()
+// import AWS from "aws-sdk"
+// import { APIGatewayProxyEvent } from "aws-lambda"
+// const s3 = new AWS.S3()
 
 const bucketName = process.env.S3_BUCKET_NAME
 
@@ -13,32 +13,32 @@ export const uploadImage = async (event: any, context: any, callback: any) => {
     // }
     console.log('received:', event)
     try {
-        if (event.httpMethod !== "POST") {
-            throw new Error(`postMethod only accepts POST  method, you tried: ${event.httpMethod} method`)
-        }
+        // if (event.httpMethod !== "POST") {
+        //     throw new Error(`postMethod only accepts POST  method, you tried: ${event.httpMethod} method`)
+        // }
 
-        const body = JSON.parse(event.body!)
-        const key = body.key
-        const s3Params = {
-            Bucket: bucketName,
-            Key: key,
-            Expires: 30000,
-            ContentType: 'image/jpeg'
-        }
+        // const body = JSON.parse(event.body!)
+        // const key = body.key
+        // const s3Params = {
+        //     Bucket: bucketName,
+        //     Key: key,
+        //     Expires: 30000,
+        //     ContentType: 'image/jpeg'
+        // }
 
-        let uploadURL = s3.getSignedUrl('putObject', s3Params)
+        // let uploadURL = s3.getSignedUrl('putObject', s3Params)
 
         callback(null,{
             statusCode: 200,
             body: JSON.stringify({
-                uploadURL: uploadURL,
-                filename: key,
+                // uploadURL: uploadURL,
+                // filename: key,
                 message: "Successfully created unsigned url to send image to S3 bucket"
             })
             ,
-            headers: {
-                "Access-Control-Allow-Origin": "*"
-            }
+            // headers: {
+                // "Access-Control-Allow-Origin": "*"
+            // }
         })
 
     } catch (err: any) {
